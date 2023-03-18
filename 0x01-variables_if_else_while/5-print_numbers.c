01494556b768ea5a30d152be341522471e621911
root <root@DESKTOP-MVR6EFL>
2023-03-18 01:18:43 +0100

    creating 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point Prints numbers of base 10 starting at 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
