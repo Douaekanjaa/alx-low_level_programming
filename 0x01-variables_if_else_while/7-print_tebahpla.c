2646cd049eb8c01ecde488e5aa3cec27b8a40e0d
root <root@DESKTOP-MVR6EFL>
2023-03-18 01:23:07 +0100

    creating 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point Prints the alphabet lowercase using ascii backwards
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
