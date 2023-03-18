88592e3550e9a082e5faba84aecfbbeefc81f1ca
root <root@DESKTOP-MVR6EFL>
2023-03-18 01:15:00 +0100

    creating 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+ #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet using ascii values.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
