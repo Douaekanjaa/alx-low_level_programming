e9876471216d14d56ab13aa4e15417594d4fe92e
root <root@DESKTOP-MVR6EFL>
2023-03-27 21:49:45 +0000

    print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev -> Prints a string, in reverse
+ *
+ * @s: Input
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
