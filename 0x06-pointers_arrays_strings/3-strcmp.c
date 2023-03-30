f54228219ce71dfa4d6de96ba3b94dcc28a3a394
root <root@DESKTOP-MVR6EFL>
2023-03-30 20:09:06 +0000

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - compare string values
+ * @str1: Input
+ * @str2: Input 
+ *
+ * Return: str1[i] - str2[i]
+ */
+int _strcmp(char *str1, char *str2)
+{
+	int i;
+
+	i = 0;
+	while (str1[i] != '\0' && str2[i] != '\0')
+	{
+		if (str1[i] != str2[i])
+		{
+			return (str1[i] - str2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+
