747776bedbce8dfdb2e270fa60d017629445bb6e
root <root@DESKTOP-MVR6EFL>
2023-04-10 22:50:26 +0000

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,9 +5,10 @@
   *@argv: array of arguments passed
   *Return: Always 0
 */
-int main(int argc, char __attribute__((unused)) *argv[]){
-	printf("%d \n", argc-1);
-	return 0;
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d \n", argc - 1);
+	return (0);
 
 }
 
