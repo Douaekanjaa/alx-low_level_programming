ee7eca108576f4f63b5c5e24eaaf0bd3bcc690aa
root <root@DESKTOP-MVR6EFL>
2023-04-10 22:43:39 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - Print the number of arguments passed into a program
+  *@argc: number of arguments passed
+  *@argv: array of arguments passed
+  *Return: Always 0
+*/
+int main(int argc, char __attribute__((unused)) *argv[]){
+	printf("The number of arguments passed is: %d \n", argc-1);
+	return 0;
+
+}
+
