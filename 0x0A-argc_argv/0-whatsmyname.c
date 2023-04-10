77b7a338bbc18d650f086c1a836101befc293f90
root <root@DESKTOP-MVR6EFL>
2023-04-10 22:23:39 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  *main - print the name of the program
+  *@argc: number if arguments passed
+  *@argv: array of arguments passed
+  *Return: Always 0
+*/
+
+int main(int __attribute__((unused)) argc, char *argv[]){
+	printf("%s\n", argv[0]);
+	return 0;
+}
+
