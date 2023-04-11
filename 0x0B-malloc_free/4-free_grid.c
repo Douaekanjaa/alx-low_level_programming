c05ad832f75034877bc2b753188e3332bca01487
Dua <youremail@example.com>
2023-04-11 15:59:55 +0000

    Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * free_grid - frees a two kdimensional greed
+  *@grid: grid
+  *@height: height of grid
+  *Return: nothing
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
