74e83896d5c2fca0af9287f277fa5d30d2ac031f
Dua <youremail@example.com>
2023-04-11 11:28:49 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
-
+/**
+  * create_array - creat an array and initialize it with a characteri
+  *@size: size of the array
+  *@c: The character to initialize with
+  *Return: a pointer, NULL if it fails
+*/
 char *create_array(unsigned int size, char c)
 {
 	char *tableau;
@@ -9,20 +14,19 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	tableau = (char *) malloc(size * sizeof(char));
-	
 	if (tableau == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
-	for (i = 0; i<size; i++)
+	for (i = 0; i < size; i++)
 	{
 		tableau[i] = c;
 	}
-	return tableau;
+	return (tableau);
 }
 
