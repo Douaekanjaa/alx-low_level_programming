bb6246e298d8202fe24cd74cbf1e9a9184c989db
Dua <youremail@example.com>
2023-04-17 15:27:48 +0000

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
-
+/**
+  * struct dog - struct with dog infos
+  * @name: the name of the dog
+  * @age: the age of the dog
+  * @owner: the owner of the dog
+*/
 struct dog
 {
 	char *name;
