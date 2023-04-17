9ebf5184047c0e218c5e13ce2d7eb7d770ad96b0
Dua <youremail@example.com>
2023-04-17 16:11:03 +0000

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 		return;
 
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-	printf("Age: %.1f\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
 
