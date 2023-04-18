c4c0c58ed5256cab08743c29a1b2e80f49135b10
Dua <youremail@example.com>
2023-04-18 22:53:15 +0000

    Task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(a) ((a) < (0) ? -(a) : (a))
+#endif
---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+
+/**
+  * main - Check the code
+  *
+  * Return: always 0
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	i = ABS(-98) * 10;
+	j = ABS(98) * 10;
+	
+	printf("%d, %d\n", i, j);
+	return (0);
+}
+
