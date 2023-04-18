795e64f98a25b3e9428a9c11a00d1a785d8b12bb
Dua <youremail@example.com>
2023-04-18 22:30:12 +0000

    Task 0

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,17 @@
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+  * main - Check the code.
+  * Return: always 0
+*/
+
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
+
