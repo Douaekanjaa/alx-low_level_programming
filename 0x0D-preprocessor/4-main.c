e8c23332281aafc7cf94142fbcc4f494cce66822
Dua <youremail@example.com>
2023-04-18 22:58:55 +0000

    Task 4

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+  * main - Check the code
+  * Return: always 0
+*/
+
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	
+	printf("%d\n", s);
+
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef SUM_H
+#define SUM_H
+
+#define SUM(a, b) ((a) + (b))
+#endif
