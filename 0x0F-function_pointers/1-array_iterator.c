beb95d2a42852f11a354c8b689836b547b30bfe2
Dua <youremail@example.com>
2023-04-19 18:38:56 +0000

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,6 +14,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i = 0;
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
 	for (i = 0; i < size; i++)
 	{
 		(*action)(array[i]);
