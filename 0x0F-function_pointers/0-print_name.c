aac1165d6ddb466c62513f3dea71e403803bc262
Dua <youremail@example.com>
2023-04-19 18:20:19 +0000

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,12 @@
 #include "function_pointers.h"
 #include <stddef.h>
+/**
+  * print_name - print a name
+  * @name: string
+  * @f: pointer to function
+  * Return: nothing
 
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
