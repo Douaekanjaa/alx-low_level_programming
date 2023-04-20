de4e1803bc8e11765805baef0fddb2095560a2fe
Dua <youremail@example.com>
2023-04-20 12:45:40 +0000

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - Search for an integer
+  * @array: array
+  * @size: the number of element in array
+  * @cmp: a pointer 
+  * Return: -1
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+
+	}
+	
+	return (-1);
+}
+
