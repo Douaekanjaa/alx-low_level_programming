b392d34a9f9dbc00a9dce66a20f41905a0edb964
Dua <youremail@example.com>
2023-04-20 16:04:07 +0000

    Task 1

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+
+/**
+  * main - Check the Code
+  * Return: always 0
+*/
+
+int main(void)
+{
+	print_numbers(",", 4, 0, 98, -1024, 402);
+
+	return (0);
+}
+
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+  * print_numbers - print numbers followed by a new line
+  * @separator: The string to be printed between numbers
+  * @n: Number of integers passed
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (separator != NULL && i != n - 1)
+		{
+			printf("%s ", separator);
+		}
+	}
+
+	printf("\n");
+
+	va_end(args);
+}
+
