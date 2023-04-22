cbc7963f08e3ea98b64d48c5339275772bc07d05
Dua <youremail@example.com>
2023-04-22 17:15:46 +0000

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,15 +9,15 @@ int helper(int n, int start, int end);
 */
 int _sqrt_recursion(int n)
 {
-        if (n < 0)
-        {
-                return (-1);
-        }
-        else if (n == 0 || n == 1)
-        {
-                return (n);
-        }
-        return (helper(n, 1, n));
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	return (helper(n, 1, n));
 }
 
 /**
