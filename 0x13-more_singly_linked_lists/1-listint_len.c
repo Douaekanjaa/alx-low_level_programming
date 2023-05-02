95f727c67d25ae9489668e755e8c5e45e7db3045
Dua <kanjaadouae1@gmail.com>
2023-05-02 17:35:43 +0100

    Task 2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * listint_len - the number of elments in a linked listint_t list.
+  * @h: pointer to listint_t 
+  * Return: The Number of elemnets in Linked list
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		c++;
+		h = h->next;
+	}
+
+	return (c);
+}
+
